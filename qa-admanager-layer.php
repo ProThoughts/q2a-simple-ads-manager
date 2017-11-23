0a95fc8385467e650abba1186e0f4ad077ccbf19
Arjun <arjun@armi.in>
2017-11-23 22:58:14 +0530

    new ad locations

--- qa-admanager-layer.php
+++ qa-admanager-layer.php
@@ -2,8 +2,16 @@
 
 
 class qa_html_theme_layer extends qa_html_theme_base {
+	function adoutput($adcode)
+	{
+		if($this->disabledcategory()) return;
+		$this->output ('<div class="adcode">'.$adcode.'</div>');
+		
+	}
+
 	function disabledcategory()
 	{
+		if($this->template !== 'question') return;
 		$categoryid = $this->content['q_view']['raw']['categoryid'];
 		$cats = explode(",",qa_opt('pt_q2a_ad_hide_categories'));
 		if(in_array($categoryid, $cats))
@@ -14,37 +22,37 @@ class qa_html_theme_layer extends qa_html_theme_base {
 	//ad after question, just before answers
 	function q_view($q_view)
 	{
+		if (qa_opt('pt_q2a_ad_before_question') && $user_level <  qa_opt('pt_q2a_ad_before_question_level')) 
+		{
+			$this->adoutput(qa_opt('pt_q2a_ad_after_question_codebox'));
+		}                     
 		qa_html_theme_base::q_view($q_view);
-		if($this->disabledcategory())
-			return;
 		$user_level = qa_get_logged_in_level();
 		if (qa_opt('pt_q2a_ad_after_question') && $user_level <  qa_opt('pt_q2a_ad_after_question_level')) 
 		{
-			$this->output(qa_opt('pt_q2a_ad_after_question_codebox'));
+			$this->adoutput(qa_opt('pt_q2a_ad_after_question_codebox'));
 		}                     
 	}
 	// End of q_view()
 
+	function head_css()
+	{
+		$this->output('<style type="text/css">'.qa_opt('pt_q2a_ad_css').' </style>');
+		qa_html_theme_base::head_css();
+	}
 	//ad after menu navigation bar, just after horizontal line.		
 	function header()
 	{
+		if (qa_opt('pt_q2a_ad_leftside') && $user_level < qa_opt('pt_q2a_ad_leftside_level')) 
+		$this->adoutput('<div class = "sidebar-ad">'.qa_opt("pt_q2a_ad_leftside_codebox"). '</div>');
 		qa_html_theme_base::header();
 		require_once QA_INCLUDE_DIR.'app/posts.php';
 		if($this -> template === 'ask' && qa_opt('pt_q2a_ad_hideaskpage'))
 			return;
-		$parts = explode("/", qa_self_html());
-		$postid = $parts[1];
-//		print_r($this -> context);
-		//$this->categoryid = qa_post_get_category($postid);
-		//$this->categoryid = 6;// qa_post_get_category($postid);
-		//print_r($this->q_view['raw']['categoryid']);
-		if($this->template === 'question')
-		if($this->disabledcategory())
-			return;
 		$user_level = qa_get_logged_in_level();
 		if (qa_opt('pt_q2a_ad_after_menu_bar') && $user_level < qa_opt('pt_q2a_ad_after_menu_bar_level')) 
 		{
-			$this->output(qa_opt('pt_q2a_ad_after_menu_bar_codebox'));
+			$this->adoutput(qa_opt('pt_q2a_ad_after_menu_bar_codebox'));
 		}                     
 	}
 	// End of header()
@@ -55,13 +63,11 @@ class qa_html_theme_layer extends qa_html_theme_base {
 		foreach ($a_items as $a_item)
 		{
 			$this->a_list_item($a_item);
-			if($this->disabledcategory())
-			continue;
 			$user_level = qa_get_logged_in_level();
 			if ($first && qa_opt('pt_q2a_ad_after_first_answer') && $user_level < qa_opt('pt_q2a_ad_after_first_answer_level') && (count($a_items) > 1 || (!qa_opt('pt_q2a_ad_after_all_answers') || $user_level >= qa_opt('pt_q2a_ad_after_all_answers_level')))) 
 			{
 				$first = false;
-				$this->output(qa_opt('pt_q2a_ad_after_first_answer_codebox'));
+				$this->adoutput(qa_opt('pt_q2a_ad_after_first_answer_codebox'));
 			}                     
 		}
 	}
@@ -72,15 +78,13 @@ class qa_html_theme_layer extends qa_html_theme_base {
 	function a_list($a_list)
 	{
 		qa_html_theme_base::a_list($a_list);
-		if($this->disabledcategory())
-			return;
 
 		$user_level = qa_get_logged_in_level();
 		if($this -> template === 'ask' && qa_opt('pt_q2a_ad_hideaskpage'))
 			return;
 		if (qa_opt('pt_q2a_ad_after_all_answers') && $user_level < qa_opt('pt_q2a_ad_after_all_answers_level')) 
 		{
-			$this->output(qa_opt('pt_q2a_ad_after_all_answers_codebox'));
+			$this->adoutput(qa_opt('pt_q2a_ad_after_all_answers_codebox'));
 		}                     
 	}
 	// end of a_list()
@@ -89,16 +93,15 @@ class qa_html_theme_layer extends qa_html_theme_base {
 	function q_list_and_form($q_list)
 	{
 
+		$this->adoutput(qa_opt('pt_q2a_ad_before_all_questions_codebox'));
 		qa_html_theme_base::q_list_and_form($q_list);
-		if($this->disabledcategory())
-			return;
 		if($this -> template === 'ask' && qa_opt('pt_q2a_ad_hideaskpage'))
 			return;
 
 		$user_level = qa_get_logged_in_level();
 		if (qa_opt('pt_q2a_ad_after_all_questions') && $user_level < qa_opt('pt_q2a_ad_after_all_questions_level')) 
 		{
-			$this->output(qa_opt('pt_q2a_ad_after_all_questions_codebox'));
+			$this->adoutput(qa_opt('pt_q2a_ad_after_all_questions_codebox'));
 		}                     
 	}
 	// end of q_list_and_form()		
@@ -106,17 +109,15 @@ class qa_html_theme_layer extends qa_html_theme_base {
 	function sidebar()
 	{
 		qa_html_theme_base::sidebar();
-		if($this->disabledcategory())
-			return;
 		if($this -> template === 'ask' && qa_opt('pt_q2a_ad_hideaskpage'))
 			return;
 
 		$user_level = qa_get_logged_in_level();
 		if (qa_opt('pt_q2a_ad_sidebar') && $user_level < qa_opt('pt_q2a_ad_sidebar_level')) 
 		{
-			$this->output(qa_opt('pt_q2a_ad_sidebar_codebox'));
+			$this->adoutput(qa_opt('pt_q2a_ad_sidebar_codebox'));
 		}                     
-	}
+	}  
 	// End of sidebar()
 
 }
--- qa-admanager-options.php
+++ qa-admanager-options.php
@@ -12,11 +12,15 @@ class pt_qa_simple_admanager {
 	function option_default($option) {
 
                 switch($option) {
+			case 'pt_q2a_ad_css': return '.sidebar-ad{position:fixed; left:0; vertical-align: top; height: 400px; width:150px;} .adcode{margin:10px;@media only screen and (max-width: 992px) {.sidebar-ad{display:none!important;}  }';
 			case 'pt_q2a_ad_after_question_level':
+			case 'pt_q2a_ad_before_question_level':
 			case 'pt_q2a_ad_after_menu_bar_level':
 			case 'pt_q2a_ad_after_all_answers_level':
 			case 'pt_q2a_ad_after_all_questions_level':
+			case 'pt_q2a_ad_before_all_questions_level':
 			case 'pt_q2a_ad_sidebar_level':
+			case 'pt_q2a_ad_leftside_level':
                                 return QA_USER_LEVEL_SUPER+1;
 			case 'pt_q2a_ad_hideaskpage': return 1;
                         default:
@@ -32,9 +36,15 @@ class pt_qa_simple_admanager {
 		$ok = null;
 		if (qa_clicked('pt_q2a_simple_ads_manager_save_button')) 
 		{
+			qa_opt('pt_q2a_ad_css', qa_post_text('pt_q2a_ad_css'));  
+ 
 			qa_opt('pt_q2a_ad_after_question',(bool)qa_post_text('pt_q2a_ad_after_question'));
 			qa_opt('pt_q2a_ad_after_question_codebox', qa_post_text('pt_q2a_ad_after_question_code_field'));   
 			qa_opt('pt_q2a_ad_after_question_level',qa_post_text('pt_q2a_ad_after_question_level'));
+			
+			qa_opt('pt_q2a_ad_before_question',(bool)qa_post_text('pt_q2a_ad_before_question'));
+			qa_opt('pt_q2a_ad_before_question_codebox', qa_post_text('pt_q2a_ad_before_question_code_field'));   
+			qa_opt('pt_q2a_ad_before_question_level',qa_post_text('pt_q2a_ad_before_question_level'));
 			
 			qa_opt('pt_q2a_ad_after_menu_bar',(bool)qa_post_text('pt_q2a_ad_after_menu_bar'));
 			qa_opt('pt_q2a_ad_after_menu_bar_codebox', qa_post_text('pt_q2a_ad_after_menu_bar_code_field'));   
@@ -43,6 +53,7 @@ class pt_qa_simple_admanager {
 			qa_opt('pt_q2a_ad_after_all_answers',(bool)qa_post_text('pt_q2a_ad_after_all_answers'));
 			qa_opt('pt_q2a_ad_after_all_answers_codebox', qa_post_text('pt_q2a_ad_after_all_answers_code_field'));   
 			qa_opt('pt_q2a_ad_after_all_answers_level',qa_post_text('pt_q2a_ad_after_all_answers_level'));
+
 			qa_opt('pt_q2a_ad_after_first_answer',(bool)qa_post_text('pt_q2a_ad_after_first_answer'));
 			qa_opt('pt_q2a_ad_after_first_answer_codebox', qa_post_text('pt_q2a_ad_after_first_answer_code_field'));   
 			qa_opt('pt_q2a_ad_after_first_answer_level',qa_post_text('pt_q2a_ad_after_first_answer_level'));
@@ -50,6 +61,10 @@ class pt_qa_simple_admanager {
 			qa_opt('pt_q2a_ad_after_all_questions',(bool)qa_post_text('pt_q2a_ad_after_all_questions'));
 			qa_opt('pt_q2a_ad_after_all_questions_codebox', qa_post_text('pt_q2a_ad_after_all_questions_code_field'));   
 			qa_opt('pt_q2a_ad_after_all_questions_level',qa_post_text('pt_q2a_ad_after_all_questions_level'));
+			
+			qa_opt('pt_q2a_ad_before_all_questions',(bool)qa_post_text('pt_q2a_ad_before_all_questions'));
+			qa_opt('pt_q2a_ad_before_all_questions_codebox', qa_post_text('pt_q2a_ad_before_all_questions_code_field'));   
+			qa_opt('pt_q2a_ad_before_all_questions_level',qa_post_text('pt_q2a_ad_before_all_questions_level'));
 			
 			qa_opt('pt_q2a_ad_sidebar',(bool)qa_post_text('pt_q2a_ad_sidebar'));
 			qa_opt('pt_q2a_ad_sidebar_codebox', qa_post_text('pt_q2a_ad_sidebar_code_field'));   
@@ -68,9 +83,12 @@ class pt_qa_simple_admanager {
         
 		qa_set_display_rules($qa_content, array(
 				
-			'pt_q2a_ad_after_question_code_display' => 'pt_q2a_ad_after_question',
 			'pt_q2a_ad_after_menu_bar_code_display' => 'pt_q2a_ad_after_menu_bar',
+			'pt_q2a_ad_before_question_code_display' => 'pt_q2a_ad_before_question',
+			'pt_q2a_ad_after_question_code_display' => 'pt_q2a_ad_after_question',
+			'pt_q2a_ad_after_first_answer_code_display' => 'pt_q2a_ad_after_first_answer',
 			'pt_q2a_ad_after_all_answers_code_display' => 'pt_q2a_ad_after_all_answers',
+			'pt_q2a_ad_before_all_questions_code_display' => 'pt_q2a_ad_before_all_questions',
 			'pt_q2a_ad_after_all_questions_code_display' => 'pt_q2a_ad_after_all_questions',
 			'pt_q2a_ad_sidebar_code_display' => 'pt_q2a_ad_sidebar',
 			'pt_q2a_ad_leftside_code_display' => 'pt_q2a_ad_leftside',
@@ -89,6 +107,15 @@ class pt_qa_simple_admanager {
 
 		$fields = array();
 		$fields[] = array(
+			'id' => 'pt_q2a_ad_css',
+			'label' => 'CSS code',
+			'type' => 'textarea',
+			'value' => qa_opt('pt_q2a_ad_css'),
+			'tags' => 'NAME="pt_q2a_ad_css"',
+            'rows' => 2,
+		);		
+		
+		$fields[] = array(
 			'label' => 'Hide Ads on Ask Page (makes reload faster)',
 			'type' => 'checkbox',
 			'value' => qa_opt('pt_q2a_ad_hideaskpage'),
@@ -101,6 +128,50 @@ class pt_qa_simple_admanager {
 			'value' => qa_opt('pt_q2a_ad_hide_categories'),
 			'tags' => 'NAME="pt_q2a_ad_hide_categories"',
             );
+		$fields[] = array(
+			'label' => 'Ad after Menu Bar',
+			'type' => 'checkbox',
+			'value' => qa_opt('pt_q2a_ad_after_menu_bar'),
+			'tags' => 'NAME="pt_q2a_ad_after_menu_bar" ID="pt_q2a_ad_after_menu_bar"',
+		);
+		
+		$fields[] = array(
+			'id' => 'pt_q2a_ad_after_menu_bar_code_display',
+			'label' => 'Paste HTML Ad Code in this box(try 728x15 text/link ad)',
+			'type' => 'textarea',
+			'value' => qa_opt('pt_q2a_ad_after_menu_bar_codebox'),
+			'tags' => 'NAME="pt_q2a_ad_after_menu_bar_code_field"',
+            'rows' => 2,
+		);		
+		$fields[] = array(
+			'label' => 'Hide This Ad for the below User Levels and Above',
+			'type' => 'select',
+			'value' => @$showoptions[qa_opt('pt_q2a_ad_after_menu_bar_level')],
+			'tags' => 'NAME="pt_q2a_ad_after_menu_bar_level" ID="pt_q2a_ad_after_menu_bar_level"',
+			'options' => $showoptions
+		);
+
+		$fields[] = array(
+			'label' => 'Ad before Question',
+			'type' => 'checkbox',
+			'value' => qa_opt('pt_q2a_ad_before_question'),
+			'tags' => 'NAME="pt_q2a_ad_before_question" ID="pt_q2a_ad_before_question"',
+		);
+		$fields[] = array(
+			'id' => 'pt_q2a_ad_before_question_code_display',
+			'label' => 'Paste HTML Ad Code in this box(try 728x90 banner ad)',
+			'type' => 'textarea',
+			'value' => qa_opt('pt_q2a_ad_before_question_codebox'),
+			'tags' => 'NAME="pt_q2a_ad_before_question_code_field"',
+            'rows' => 2,
+		);
+		$fields[] = array(
+			'label' => 'Hide This Ad for the below User Levels and Above',
+			'type' => 'select',
+			'value' => @$showoptions[qa_opt('pt_q2a_ad_before_question_level')],
+			'tags' => 'NAME="pt_q2a_ad_before_question_level" ID="pt_q2a_ad_before_question_level"',
+			'options' => $showoptions
+		);
 
 		$fields[] = array(
 			'label' => 'Ad after Question',
@@ -125,28 +196,6 @@ class pt_qa_simple_admanager {
 			'options' => $showoptions
 		);
 
-		$fields[] = array(
-			'label' => 'Ad after Menu Bar',
-			'type' => 'checkbox',
-			'value' => qa_opt('pt_q2a_ad_after_menu_bar'),
-			'tags' => 'NAME="pt_q2a_ad_after_menu_bar" ID="pt_q2a_ad_after_menu_bar"',
-		);
-		
-		$fields[] = array(
-			'id' => 'pt_q2a_ad_after_menu_bar_code_display',
-			'label' => 'Paste HTML Ad Code in this box(try 728x15 text/link ad)',
-			'type' => 'textarea',
-			'value' => qa_opt('pt_q2a_ad_after_menu_bar_codebox'),
-			'tags' => 'NAME="pt_q2a_ad_after_menu_bar_code_field"',
-            'rows' => 2,
-		);		
-		$fields[] = array(
-			'label' => 'Hide This Ad for the below User Levels and Above',
-			'type' => 'select',
-			'value' => @$showoptions[qa_opt('pt_q2a_ad_after_menu_bar_level')],
-			'tags' => 'NAME="pt_q2a_ad_after_menu_bar_level" ID="pt_q2a_ad_after_menu_bar_level"',
-			'options' => $showoptions
-		);
 
 		$fields[] = array(
 			'label' => 'Ad after All Answers',
@@ -193,6 +242,29 @@ class pt_qa_simple_admanager {
 			'options' => $showoptions
 		);
 		
+		$fields[] = array(
+			'label' => 'Ad before All Questions',
+			'type' => 'checkbox',
+			'value' => qa_opt('pt_q2a_ad_before_all_questions'),
+			'tags' => 'NAME="pt_q2a_ad_before_all_questions" ID="pt_q2a_ad_before_all_questions"',
+		);
+		
+		$fields[] = array(
+			'id' => 'pt_q2a_ad_before_all_questions_code_display',
+			'label' => 'Paste HTML Ad Code in this box(try 728x90 banner ad)',
+			'type' => 'textarea',
+			'value' => qa_opt('pt_q2a_ad_before_all_questions_codebox'),
+			'tags' => 'NAME="pt_q2a_ad_before_all_questions_code_field"',
+            'rows' => 2,
+		);		
+		$fields[] = array(
+			'label' => 'Hide This Ad for the below User Levels and Above',
+			'type' => 'select',
+			'value' => @$showoptions[qa_opt('pt_q2a_ad_before_all_questions_level')],
+			'tags' => 'NAME="pt_q2a_ad_before_all_questions_level" ID="pt_q2a_ad_before_all_questions_level"',
+			'options' => $showoptions
+		);
+
 		$fields[] = array(
 			'label' => 'Ad after All Questions',
 			'type' => 'checkbox',
